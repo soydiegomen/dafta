534bc195665398f96872b6c3652fd01d17311eb1
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-07-09 22:12:23 -0500

    Primera versión de POC para hacer navegación entre secciones utilizando el scroll.

--- js/custom/home.js
+++ js/custom/home.js
@@ -4,7 +4,7 @@ $(function() {
 
 	function nextSection(){
 		var next = current + 1;
-		console.log(current+" - " +next);
+		console.log("next section");
 		$('.section'+current).removeClass('current');
 		$('body').scrollTo('.section'+next, {duration:600}, {queue:true});
 		$('.section'+next).addClass('current');
@@ -13,6 +13,7 @@ $(function() {
 	function previousSection(){
 		//SCROLL UP
 		var prev = current - 1;
+		console.log("prev section");
 		$('.section'+current).removeClass('current');
 		$('body').scrollTo( '.section' + prev, 600, {queue:true} );
 		$('.section'+prev).addClass('current');	
@@ -22,7 +23,6 @@ $(function() {
 	$(document).keydown(function(e){
 		current = $('div.current').data('section');
 		if (e.keyCode == 40) {
-			console.log("down");
 			//SCROLL DOWN
 			if(current < noSections){
 
@@ -32,7 +32,6 @@ $(function() {
 			}
 		} 
 		else if (e.keyCode == 38) {
-			console.log("up");
 			//SCROLL UP
 			if(current > 1){
 				previousSection();
@@ -41,11 +40,69 @@ $(function() {
 			}
 		}
 	});
-
+	//POC for change section using scroll. The proble is when interact up and down scroll
+	/*
 	var lastScrollTop = 0;
+	var lastChangeSection = 0;
+	var factorMovimiento = 40;
+	var lastMoveNext = 0;
+	var lastMovePrev = 0;
+	var startPrev = 150;
+	var endPrev = 350;
+	var startNext = 0;
+	var endNext = 200;
 	$( window ).scroll(function() {
-		var st = window.pageYOffset || document.documentElement.scrollTop;
-	  	//console.log(lastScrollTop + ' - ' + st);
-	  	lastScrollTop = st;
+		var currentStrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		current = $('div.current').data('section');
+		console.log(currentStrollTop);
+		if(currentStrollTop >= 600){
+			console.log("Start!");
+			lastMovePrev = 600;
+		}
+	  	
+	  	// 150 > 50
+	  	if(currentStrollTop > startPrev && currentStrollTop < endPrev){
+		  	if((lastMovePrev - factorMovimiento) > currentStrollTop){
+		  		console.log("Is up");
+		  		//console.log(current +" - " + noSections);
+		  		if(true){
+			  		//previousSection();
+			  		moveToPrev();
+			  		console.log("Prev move:" + lastMovePrev);
+			  		console.log("Next move:" + lastMoveNext);
+			  	}
+		  		
+		  	}
+		}
+	  	if(currentStrollTop > startNext && currentStrollTop < endNext){
+		  	if( currentStrollTop > (lastMoveNext + factorMovimiento)){
+		  		console.log("Is down");
+		  		//console.log(current +" - " + noSections);
+		  		if(true){
+			  		//nextSection();
+			  		moveToNext();
+			  		console.log("Next move:" + lastMoveNext);
+			  		console.log("Prev move:" + lastMovePrev);
+			  	}
+		  		
+		  	}
+	  	}
+		
+	  	lastScrollTop = currentStrollTop;
+	  	//e.preventDefault();
+		//return false;
 	});
+
+	function moveToNext(){
+		console.log("Move to next");
+		lastMoveNext += 200;
+		lastMovePrev = lastMoveNext;
+	}
+
+	function moveToPrev(){
+		console.log("Move to prev");	
+		lastMovePrev -= 200;
+		lastMoveNext = lastMovePrev;
+	}
+	*/
 });
